c834d54aa5db68d07e28f1c41cef7ae88c1b4621
akitayuri <nmnl_y.bgux@icloud.com>
2021-07-29 15:25:57 +0900

    [add] ユーザーマイページの作成, リンク追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :address, :teeth_type, :orthodontics_type])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :address, :teeth_type, :orthodontics_type])
   end
+
+  def after_sign_in_path_for(resource)
+    user_path(resource.id)
+  end
 end
