1bca0a5eddcb73c198fdcf995e102926d23df667
akitayuri <nmnl_y.bgux@icloud.com>
2021-07-30 13:53:10 +0900

    ルート追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,15 @@
 Rails.application.routes.draw do
-  get 'blogs/index'
-  get 'blogs/new'
-  get 'blogs/create'
-  get 'blogs/edit'
-  get 'blogs/update'
-  get 'blogs/destroy'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers: {
     registrations: "users/registrations",
   }
+
   root to: 'top#index'
   resources :users, only: [:show]
+  resources :blogs
+
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
+
 end
