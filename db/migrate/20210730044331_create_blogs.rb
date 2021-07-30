35967f4c471baaa65915a2db009391640f589b9d
akitayuri <nmnl_y.bgux@icloud.com>
2021-07-30 13:49:07 +0900

    [add] schemaにblogsテーブルを反映

--- db/migrate/20210730044331_create_blogs.rb
+++ db/migrate/20210730044331_create_blogs.rb
@@ -1,7 +1,7 @@
 class CreateBlogs < ActiveRecord::Migration[6.1]
   def change
     create_table :blogs do |t|
-      t.text :content
+      t.text :content, nill: false
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
