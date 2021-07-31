65ef8f056d7aba2e7af1ad70d8ba5e252e8e7216
akitayuri <nmnl_y.bgux@icloud.com>
2021-08-01 04:17:49 +0900

    [add] likesテーブルをschemaへ追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_31_164250) do
+ActiveRecord::Schema.define(version: 2021_07_31_191422) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,15 @@ ActiveRecord::Schema.define(version: 2021_07_31_164250) do
     t.index ["user_id"], name: "index_bookmarks_on_user_id"
   end
 
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "review_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["review_id"], name: "index_likes_on_review_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
   create_table "reviews", force: :cascade do |t|
     t.datetime "reservation_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
     t.integer "quote_price", null: false
@@ -99,5 +108,7 @@ ActiveRecord::Schema.define(version: 2021_07_31_164250) do
   add_foreign_key "blogs", "users"
   add_foreign_key "bookmarks", "blogs"
   add_foreign_key "bookmarks", "users"
+  add_foreign_key "likes", "reviews"
+  add_foreign_key "likes", "users"
   add_foreign_key "reviews", "users"
 end
