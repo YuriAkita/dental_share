c6efbe5dcc0407fc4553d6f296f026f50eccee4f
akitayuri <nmnl_y.bgux@icloud.com>
2021-07-31 15:09:26 +0900

    [add] reviewsテーブル生成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_30_044331) do
+ActiveRecord::Schema.define(version: 2021_07_31_050831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,18 @@ ActiveRecord::Schema.define(version: 2021_07_30_044331) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_blogs_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.datetime "reservation_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.integer "quote_price", null: false
+    t.integer "orthodontics_type", default: 0, null: false
+    t.text "content", null: false
+    t.integer "star", default: 0, null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -46,4 +58,5 @@ ActiveRecord::Schema.define(version: 2021_07_30_044331) do
   end
 
   add_foreign_key "blogs", "users"
+  add_foreign_key "reviews", "users"
 end
