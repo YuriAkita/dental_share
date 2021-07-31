98e3c641d29de354d0abfa4eb44fced463e20f05
akitayuri <nmnl_y.bgux@icloud.com>
2021-07-31 20:51:23 +0900

    [add] ユーザーのプロフィール画像設定追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :address, :teeth_type, :orthodontics_type])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :address, :teeth_type, :orthodontics_type])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :address, :teeth_type, :orthodontics_type, :profile_image])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :address, :teeth_type, :orthodontics_type, :profile_image])
   end
 
   def after_sign_in_path_for(resource)
