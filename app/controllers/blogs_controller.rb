52b340e1d14d84d301a79d49c47aba16b7f019a3
akitayuri <nmnl_y.bgux@icloud.com>
2021-07-31 13:46:53 +0900

    [add] blogsコントローラ設定 ビュー作成

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,19 +1,59 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: %i[show edit update destroy]
+
   def index
+    @blogs = Blog.all
   end
 
   def new
+    @blog = Blog.new
   end
 
   def create
+    @blog = current_user.blogs.build(blog_params)
+    if params[:back]
+      render :new
+    else
+      if @blog.save
+        redirect_to blogs_path, notice: "ブログを作成しました！"
+      else
+        render :new
+      end
+    end
   end
 
   def edit
   end
 
+  def show
+  end
+
   def update
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice: "ブログを編集しました！"
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @blog.destroy
+    redirect_to blogs_path, notice:"ブログを削除しました！"
   end
+
+  def confirm
+    @blog = current_user.blogs.build(blog_params)
+    render :new if @blog.invalid?
+  end
+
+  private
+
+  def set_blog
+    @blog = Blog.find(params[:id])
+  end
+
+  def blog_params
+    params.require(:blog).permit(:content, :user_id)
+  end
+
 end
