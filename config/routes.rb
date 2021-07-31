0be858226e578a83b8751473c2eea0af1d88050a
akitayuri <nmnl_y.bgux@icloud.com>
2021-08-01 00:34:59 +0900

    [add] ブログの画像投稿設定追加

--- config/routes.rb
+++ config/routes.rb
@@ -7,17 +7,8 @@ Rails.application.routes.draw do
 
   root to: 'top#index'
   resources :users, only: [:show]
-  resources :reviews do
-    collection do
-      post :confirm
-    end
-  end
-  
-  resources :blogs do
-    collection do
-      post :confirm
-    end
-  end
+  resources :reviews 
+  resources :blogs
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
