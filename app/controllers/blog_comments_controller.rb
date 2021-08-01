d9ad9f37bd777525f3ecbb21035f96d1b36c4ab1
akitayuri <nmnl_y.bgux@icloud.com>
2021-08-02 00:38:52 +0900

    [add] blog_commentsコントローラのcreateメソッド追加, ブログ詳細ページ一部コメント用の記述追加

--- app/controllers/blog_comments_controller.rb
+++ app/controllers/blog_comments_controller.rb
@@ -1,4 +1,18 @@
 class BlogCommentsController < ApplicationController
   def create
+    @blog = Blog.find(params[:blog_id])
+    @blog_comment = @blog.blog_comments.build(blog_comment_params)
+    respond_to do |format|
+      if @blog_comment.save
+        format.html { redirect_to blog_path(@blog) }
+      else
+        format.html { redirect_to blog_path(@blog), notice: '投稿できませんでした...' }
+      end
+    end
+  end
+
+  private
+  def blog_comment_params
+    params.require(:comment).permit(:blog_id, :user_id, :content)
   end
 end
