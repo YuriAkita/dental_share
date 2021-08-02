e86fd688f8496da1720d5bc81531f7782c428275
akitayuri <nmnl_y.bgux@icloud.com>
2021-08-02 19:37:27 +0900

    [add] 口コミコメント用のコントローラ作成

---
+++ spec/requests/review_comments_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "ReviewComments", type: :request do
+  describe "GET /create" do
+    it "returns http success" do
+      get "/review_comments/create"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
