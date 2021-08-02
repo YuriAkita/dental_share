032a3b541bf69d2e65716c568a576281651001e1
akitayuri <nmnl_y.bgux@icloud.com>
2021-08-02 19:51:03 +0900

    [add] 口コミコメント新規投稿できるようにコントローラ・ビューの追加

--- app/controllers/review_comments_controller.rb
+++ app/controllers/review_comments_controller.rb
@@ -1,4 +1,19 @@
 class ReviewCommentsController < ApplicationController
   def create
+    @review = Review.find(params[:review_id])
+    @review_comment = @review.review_comments.build(review_comment_params)
+    @review_comment.user_id = current_user.id
+    respond_to do |format|
+      if @review_comment.save
+        format.html { redirect_to review_path(@review) }
+      else
+        format.html { redirect_to review_path(@review), notice: '投稿できませんでした...' }
+      end
+    end
+  end
+
+  private
+  def review_comment_params
+    params.require(:review_comment).permit(:review_id, :user_id, :content)
   end
 end
