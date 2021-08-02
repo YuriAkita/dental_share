63744124f29c9782a286a28234619fb99ca04ade
akitayuri <nmnl_y.bgux@icloud.com>
2021-08-02 19:31:07 +0900

    [add] 外部キー入力漏れを追記

--- db/migrate/20210802102136_create_review_comments.rb
+++ db/migrate/20210802102136_create_review_comments.rb
@@ -1,6 +1,8 @@
 class CreateReviewComments < ActiveRecord::Migration[6.1]
   def change
     create_table :review_comments do |t|
+      t.references :review, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
       t.text :content
 
       t.timestamps
