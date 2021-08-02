e829dd71a5185d26a21dfdcb204a770dbf90dac6
akitayuri <nmnl_y.bgux@icloud.com>
2021-08-02 20:10:41 +0900

    [add] 口コミコメント編集・削除機能追加

--- app/controllers/review_comments_controller.rb
+++ app/controllers/review_comments_controller.rb
@@ -12,6 +12,40 @@ class ReviewCommentsController < ApplicationController
     end
   end
 
+  def edit
+    @review = Review.find(params[:review_id])
+    @review_comment = @review.review_comments.find(params[:id])
+    @review_comment.user_id = current_user.id
+    respond_to do |format|
+      flash.now[:notice] = 'コメントの編集中'
+      format.js { render :edit }
+    end
+  end
+
+  def update
+    @review = Review.find(params[:review_id])
+    @review_comment = @review.review_comments.find(params[:id])
+    @review_comment.user_id = current_user.id
+      respond_to do |format|
+        if @review_comment.update(review_comment_params)
+          flash.now[:notice] = 'コメントが編集されました'
+          format.js { render :index }
+        else
+          flash.now[:notice] = 'コメントの編集に失敗しました'
+          format.js { render :edit_error }
+        end
+      end
+  end
+
+  def destroy
+    @review_comment = ReviewComment.find(params[:id])
+    @review_comment.destroy
+    respond_to do |format|
+      flash.now[:notice] = 'コメントが削除されました'
+      format.js { render :index }
+    end
+  end
+
   private
   def review_comment_params
     params.require(:review_comment).permit(:review_id, :user_id, :content)
