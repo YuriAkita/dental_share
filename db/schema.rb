050e2424a2ed4c625c3d9e3b287aafbfe69116b9
akitayuri <nmnl_y.bgux@icloud.com>
2021-08-03 14:55:29 +0900

    [add] clinicモデル,seedデータ追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_02_102136) do
+ActiveRecord::Schema.define(version: 2021_08_03_055154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,13 @@ ActiveRecord::Schema.define(version: 2021_08_02_102136) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["blog_id"], name: "index_bookmarks_on_blog_id"
     t.index ["user_id"], name: "index_bookmarks_on_user_id"
+  end
+
+  create_table "clinics", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "likes", force: :cascade do |t|
