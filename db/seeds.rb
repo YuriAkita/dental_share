461075eec1545c5a1acbd0f0938a02681b896b1e
akitayuri <nmnl_y.bgux@icloud.com>
2021-08-09 10:58:48 +0900

    [add] blog seedデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,7 +25,7 @@ User.create!(
   address: 2,
   teeth_type: 3,
   orthodontics_type: 8,
-  profile_image:ActiveStorage::Blob.create_and_upload!(io: File.open("./db/images/woman1.jpg"), filename: "woman1.jpg")
+  profile_image:ActiveStorage::Blob.create_and_upload!(io: File.open("./db/images/woman1.jpg"), filename: "woman1.png")
 )
 
 User.create!(
@@ -36,7 +36,7 @@ User.create!(
   address: 9,
   teeth_type: 2,
   orthodontics_type: 7,
-  profile_image:ActiveStorage::Blob.create_and_upload!(io: File.open("./db/images/woman2.jpg"), filename: "woman2.jpg")
+  profile_image:ActiveStorage::Blob.create_and_upload!(io: File.open("./db/images/woman2.jpg"), filename: "woman2.png")
 )
 
 User.create!(
@@ -47,7 +47,7 @@ User.create!(
   address: 1,
   teeth_type: 2,
   orthodontics_type: 1,
-  profile_image:ActiveStorage::Blob.create_and_upload!(io: File.open("./db/images/woman3.jpg"), filename: "woman3.jpg")
+  profile_image:ActiveStorage::Blob.create_and_upload!(io: File.open("./db/images/woman3.jpg"), filename: "woman3.png")
 )
 
 User.create!(
@@ -69,4 +69,34 @@ User.create!(
   address: 1,
   teeth_type: 2,
   orthodontics_type: 2,  profile_image:ActiveStorage::Blob.create_and_upload!(io: File.open("./db/images/man1.jpg"), filename: "man1.jpg")
+)
+
+Blog.create!(
+  user_id: 2,
+  content: "矯正1日目！口の中が違和感、、",
+  images:ActiveStorage::Blob.create_and_upload!(io: File.open("./db/images/teeth2.jpg"), filename: "teeth2.jpg")
+)
+
+Blog.create!(
+  user_id: 2,
+  content: "矯正2日目！毎回取り外した時の開放感。口内炎ができて痛い。",
+  images:ActiveStorage::Blob.create_and_upload!(io: File.open("./db/images/teeth4.jpg"), filename: "teeth4.jpg")
+)
+
+Blog.create!(
+  user_id: 3,
+  content: "今日から2枚目。まあまあの圧迫感。",
+  images:ActiveStorage::Blob.create_and_upload!(io: File.open("./db/images/teeth.jpg"), filename: "teeth.jpg")
+)
+
+Blog.create!(
+  user_id: 3,
+  content: "歯が動いているのか、噛み合わせが変わってきて食べるときに違和感がある。",
+  images:ActiveStorage::Blob.create_and_upload!(io: File.open("./db/images/teeth.jpg"), filename: "teeth.jpg")
+)
+
+Blog.create!(
+  user_id: 6,
+  content: "前歯は綺麗になってきたけど、奥歯の方がまだ隙間があって気になる。",
+  images:ActiveStorage::Blob.create_and_upload!(io: File.open("./db/images/teeth3.jpg"), filename: "teeth3.jpg")
 )
