453b7436c167c3a4021bc1461f17128c483a95e6
akitayuri <nmnl_y.bgux@icloud.com>
2021-08-09 16:16:55 +0900

    [add] review, review_comment, like seedデータ追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,8 +24,8 @@ User.create!(
   admin: false,
   address: 2,
   teeth_type: 3,
-  orthodontics_type: 8,
-  profile_image:ActiveStorage::Blob.create_and_upload!(io: File.open("./db/images/woman1.jpg"), filename: "woman1.png")
+  orthodontics_type: 7,
+  profile_image:ActiveStorage::Blob.create_and_upload!(io: File.open("./db/images/woman1.png"), filename: "woman1.png")
 )
 
 User.create!(
@@ -34,9 +34,9 @@ User.create!(
   password: "password3",
   admin: false,
   address: 9,
-  teeth_type: 2,
-  orthodontics_type: 7,
-  profile_image:ActiveStorage::Blob.create_and_upload!(io: File.open("./db/images/woman2.jpg"), filename: "woman2.png")
+  teeth_type: 1,
+  orthodontics_type: 8,
+  profile_image:ActiveStorage::Blob.create_and_upload!(io: File.open("./db/images/woman2.png"), filename: "woman2.png")
 )
 
 User.create!(
@@ -47,7 +47,7 @@ User.create!(
   address: 1,
   teeth_type: 2,
   orthodontics_type: 1,
-  profile_image:ActiveStorage::Blob.create_and_upload!(io: File.open("./db/images/woman3.jpg"), filename: "woman3.png")
+  profile_image:ActiveStorage::Blob.create_and_upload!(io: File.open("./db/images/woman3.png"), filename: "woman3.png")
 )
 
 User.create!(
@@ -99,4 +99,160 @@ Blog.create!(
   user_id: 6,
   content: "前歯は綺麗になってきたけど、奥歯の方がまだ隙間があって気になる。",
   images:ActiveStorage::Blob.create_and_upload!(io: File.open("./db/images/teeth3.jpg"), filename: "teeth3.jpg")
+)
+
+BlogComment.create!(
+  blog_id: 1,
+  user_id: 4,
+  content: "一緒に頑張りましょう！"
+)
+
+BlogComment.create!(
+  blog_id: 1,
+  user_id: 5,
+  content: "いいね！"
+)
+
+BlogComment.create!(
+  blog_id: 4,
+  user_id: 6,
+  content: "わかります(TωT)"
+)
+
+BlogComment.create!(
+  blog_id: 2,
+  user_id: 3,
+  content: "口内炎はつらい。"
+)
+
+BlogComment.create!(
+  blog_id: 2,
+  user_id: 4,
+  content: "ご飯食べるのが辛くなります。"
+)
+
+Bookmark.create!(
+  blog_id: 5,
+  user_id: 2
+)
+
+Bookmark.create!(
+  blog_id: 5,
+  user_id: 3
+)
+
+Bookmark.create!(
+  blog_id: 5,
+  user_id: 4
+)
+
+Bookmark.create!(
+  blog_id: 4,
+  user_id: 4
+)
+
+Bookmark.create!(
+  blog_id: 4,
+  user_id: 3
+)
+
+Review.create!(
+  user_id: 2,
+  clinic_id: 201,
+  quote_price: 96,
+  orthodontics_type: 8,
+  content: "カウンセリングにいってきましたが3Dシュミレーションがすごかった。親知らずがあるので親知らずの相談もしてきました。矯正期間は1年半程度と言われました。",
+  star: 3
+)
+
+Review.create!(
+  user_id: 3,
+  clinic_id: 100,
+  quote_price: 40,
+  orthodontics_type: 7,
+  content: "コロナでマスク生活をしている間に矯正しようと思いカウンセリングに行きました。私は前歯だけだったのでiGOを勧められました。医師の方もキチンと話を聞いてくれ安心しました。まずは大学病院で親知らずを抜く必要があるそうです。",
+  star: 3
+)
+
+Review.create!(
+  user_id: 4,
+  clinic_id: 55,
+  quote_price: 35,
+  orthodontics_type: 7,
+  content: "歯と歯の隙間に間を作るために紙一枚分程度に歯の間をやすりがけするらしいです。計画も丁寧に話してくれて安心できました。早く綺麗な歯並びになりたい。",
+  star: 2
+)
+
+Review.create!(
+  user_id: 5,
+  clinic_id: 20,
+  quote_price: 100,
+  orthodontics_type: 7,
+  content: "病院内はとても綺麗で落ち着いていました。もし通うことになったら通いやすそう。説明も丁寧でした。親知らず含め抜歯も勧められました。今治療中の虫歯についても聞いてみましたが虫歯の治療が終わってから矯正治療を進めるそうです。",
+  star: 3
+)
+
+Review.create!(
+  user_id: 6,
+  clinic_id: 10,
+  quote_price: 95,
+  orthodontics_type: 2,
+  content: "ワイヤー矯正とマウスピースで迷っていましたが、とても丁寧に説明していただけた。期間は2年程度と言われました。",
+  star: 3
+)
+
+
+ReviewComment.create!(
+  review_id: 1,
+  user_id: 4,
+  content: "親知らず何本抜くんですか？"
+)
+
+ReviewComment.create!(
+  review_id: 1,
+  user_id: 5,
+  content: "いいね！"
+)
+
+ReviewComment.create!(
+  review_id: 4,
+  user_id: 6,
+  content: "私も虫歯があるんですけど矯正がしたかったので参考になりました。"
+)
+
+ReviewComment.create!(
+  review_id: 2,
+  user_id: 3,
+  content: "矯正の種類って色々あるんですね"
+)
+
+ReviewComment.create!(
+  review_id: 2,
+  user_id: 4,
+  content: "抜歯辛い"
+)
+
+Like.create!(
+  review_id: 5,
+  user_id: 2
+)
+
+Like.create!(
+  review_id: 5,
+  user_id: 3
+)
+
+Like.create!(
+  review_id: 5,
+  user_id: 4
+)
+
+Like.create!(
+  review_id: 4,
+  user_id: 4
+)
+
+Like.create!(
+  review_id: 4,
+  user_id: 3
 )
