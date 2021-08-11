2514e5e018560e06e8cde2192d78ffb225f56533
akitayuri <nmnl_y.bgux@icloud.com>
2021-08-12 03:33:54 +0900

    [add] validate カウンセリング日の未来日付の入力制限 見積もり金額の数値範囲指定

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,15 @@
+class Admin::UsersController < ApplicationController
+  before_action :if_not_admin
+  def new
+    @user = User.new
+  end
+
+  private
+  def if_not_admin
+    unless current_user.admin?
+      redirect_to root_path
+      flash[:notice]="管理者以外はアクセスできません"
+    end
+  end
+
+end
