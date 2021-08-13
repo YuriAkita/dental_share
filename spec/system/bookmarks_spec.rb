bb9702d72ecf820f3548604da79aae608e31ab8a
akitayuri <nmnl_y.bgux@icloud.com>
2021-08-13 20:38:41 +0900

    [add] bookmarks system_spec追加

--- spec/system/bookmarks_spec.rb
+++ spec/system/bookmarks_spec.rb
@@ -1,4 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe "Bookmarks", type: :system do
+  let!(:user) { FactoryBot.create(:user) }
+  let!(:admin_user) { FactoryBot.create(:admin_user) }
+  let(:blog) { FactoryBot.create(:blog, user: user) }
+
+  describe '日記のブックマーク機能' do
+
+    before do
+      visit new_user_session_path
+      fill_in 'user[email]', with: 'test1@email.com'
+      fill_in 'user[password]', with: 'test123'
+      click_button 'commit'
+    end
+
+    context '自分以外の日記の詳細画面でブックマークをした場合' do
+      it '日記ブックマーク一覧にブックマークをした日記が表示される' do
+        blog = FactoryBot.create(:blog, content: "blog_bookmark_test", user: admin_user)
+        visit blogs_path
+        click_link "blog_index_show-#{blog.id}"
+        click_link "ブックマークする"
+        visit bookmarks_path
+        expect(page).to have_content 'blog_bookmark_test'
+      end
+    end
+  end
+
+
 end
