8ed536cda0506e800f22b4abf2ce300d009c1071
akitayuri <nmnl_y.bgux@icloud.com>
2021-08-14 21:47:02 +0900

    [add] review_comment models spec 追加

--- spec/models/review_comment_spec.rb
+++ spec/models/review_comment_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe ReviewComment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'レビューコメント機能' do
+    context 'レビューコメントのcontentが空の場合' do
+      it 'バリデーションが通らない' do
+        user = FactoryBot.create(:user)
+        review = review = Review.new(user: user, content: "test", star: 1, orthodontics_type: 1, quote_price: 1, reservation_at: "002020-07-01")
+        review_comment = ReviewComment.new(content: "")
+        expect(review_comment).not_to be_valid
+      end
+    end
+  end
 end
