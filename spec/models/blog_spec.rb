e316fd3abc14805c8989f74d603baff650b59b83
akitayuri <nmnl_y.bgux@icloud.com>
2021-08-14 21:22:36 +0900

    [add] blog models spec 追加

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Blog, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'ブログ機能' do
+    context 'ブログのcontentが空の場合' do
+      it 'バリデーションが通らない' do
+        user = FactoryBot.create(:user)
+        blog = Blog.new(user: user, content: "", images:ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/spec/fixtures/image/teeth3.jpg"), filename: 'teeth3.jpg'))
+        expect(blog).not_to be_valid
+      end
+    end
+
+    context 'ブログのimagesが空の場合' do
+      it 'バリデーションが通らない' do
+        user = FactoryBot.create(:user)
+        blog = Blog.new(user: user, content: "test", images: nil)
+      end
+    end
+  end
 end
