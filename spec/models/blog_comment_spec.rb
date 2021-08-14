117b8c2ac48229a07c56188941c3467aaf6dae31
akitayuri <nmnl_y.bgux@icloud.com>
2021-08-14 21:27:32 +0900

    [add] blog_comment models spec追加

--- spec/models/blog_comment_spec.rb
+++ spec/models/blog_comment_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe BlogComment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'ブログコメント機能' do
+    context 'ブログコメントのcontentが空の場合' do
+      it 'バリデーションが通らない' do
+        user = FactoryBot.create(:user)
+        blog = FactoryBot.create(:blog, user: user)
+        blog_comment = BlogComment.new(content: "")
+      end
+    end
+  end
 end
