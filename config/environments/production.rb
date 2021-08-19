72a25e615d5e45c4db4e8dab6fdbceec16f17e6b
akitayuri <nmnl_y.bgux@icloud.com>
2021-08-19 15:01:50 +0900

    [fix] ES6の構文を使用するために環境設定変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(:harmony => true)
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
